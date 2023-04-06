b1b8d57d0291daadfe554a50cb3cbe043d7a98b9
Ankit Kumar <92395053+ankittt20@users.noreply.github.com>
2023-04-06 18:48:01 +0530

    Add files via upload

---
+++ eventData.js
@@ -0,0 +1,201 @@
+var eventData = [];
+export default eventData = [
+  {
+    id: 1,
+    category: "technical",
+    title: "AAKRITI",
+    description:
+      "AAKRITI, which literally means 'creation', is organised by the Civil engineering department of NIT Jamshedpur under OJASS. Being true to its name and motto, Aakriti calls out to all the budding civil engineers to utilize their potential and work practically towards the problems in the modern world.",
+    imageUrl:
+      "https://images.pexels.com/photos/3771045/pexels-photo-3771045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 2,
+    category: "technical",
+    title: "CIRCUIT HOUSE",
+    description:
+      "With the onset of various brainstorming innovations in the field of power generation and electricity consumption, the fundamental principles and theories of electrical engineering remain intact and continue to serve as the backbone of these technologies. CIRCUIT HOUSE organised under OJASS 2023 invites young minds to explore and develop their intellect with regard to electrical engineering and its subsidiaries.",
+    imageUrl:
+      "https://images.pexels.com/photos/163100/circuit-circuit-board-resistor-computer-163100.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 3,
+    category: "entrepreneurial",
+    title: "PRAYAS",
+    description:
+      "Prayas (by NSS NIT JAMSHEDPUR) organized under OJASS’23 is all about unleashing your creativity and critical thinking skills to come up with practical solutions to the problems around you. With a focus on social and cultural issues, Prayas challenges you to analyze and suggest solutions to some of the most pressing problems plaguing modern India.",
+    imageUrl:
+      "https://images.pexels.com/photos/973506/pexels-photo-973506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 4,
+    category: "technical",
+    title: "VISHWA CODE GENESIS",
+    description:
+      "With VishwaCode Genesis, the Computer Science Branch under OJASS’23 is proud to present an unparalleled opportunity for developers, programmers, and tech experts to showcase their skills and talents. Buckle up for an epic battle of programmers under the VishwaCode Genesis and get ready to showcase your skills and compete with the best of the best.",
+    imageUrl:
+      "https://images.pexels.com/photos/3861951/pexels-photo-3861951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 5,
+    category: "miscellaneous",
+    title: "NCFF",
+    description:
+      "Photography is the art of teleporting the past into the future. Phocus presents the National College Film Festival - a combination of events, from filmmaking, reel making, after movie to Ad making. So bring out your perfect angles and creativity and participate in this event OJASS’23, filled lights, cameras and passion!",
+    imageUrl:
+      "https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 6,
+    title: "AAVARTAN",
+    category: "technical",
+    description:
+      "Ojass'23 brings you 'AAVARTAN'  an opportunity to showcase your different skills related to field of metallurgy and others as well, consists events like quizess ,poster competitions , interview experience and more brain teasures and fun events.",
+    imageUrl:
+      "https://images.pexels.com/photos/355948/pexels-photo-355948.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 7,
+    category: "entrepreneurial",
+    title: "METAPHORICA",
+    description:
+      "Metaphorica is a literary segment conducted by the Literary and Debating Society under the aegis of OJASS - The annual Techno-Management Fest of NIT Jamshedpur. It is a three-day extravaganza to satisfy the appetite of all the literary nerds out there. So, let the ‘metamorphosis’ happen and get captivated by the cognitive challenge we bring to you.",
+    imageUrl:
+      "https://images.pexels.com/photos/8847004/pexels-photo-8847004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 8,
+    category: "entrepreneurial",
+    title: "ARTHASHASTRA",
+    description:
+      "Arthashastra is the ultimate entrepreneurial event with a vision to inspire and empower the next generation of leaders . Over the course of three exhilarating days, be prepared to be awed and challenged to our intense case study analysis and a series of competitions that will put your entrepreneurial prowess to the test. With Arthashastra, the sky's the limit!",
+    imageUrl:
+      "https://images.pexels.com/photos/4559592/pexels-photo-4559592.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 9,
+    category: "technical",
+    title: "SILICON VALLEY",
+    description:
+      "One of India’s most notable techno-management fests, OJASS is here and we at the S.E.C.E. (Society of Electronics and Communications Engineering) are contributing to this massive fest with our very own series of events: Silicon Valley. Silicon Valley consists of 7 events that are based on electronics, programming, etc. Through these events, we aim to draw attention to various technologies that are used at an industry level, all while promoting an entertaining and competitive environment.",
+    imageUrl:
+      "https://images.pexels.com/photos/14243437/pexels-photo-14243437.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 10,
+    category: "technical",
+    title: "NEO DRISHTI",
+    description:
+      "Neo Drishti under OJASS’23 calls out to all the coding geeks who want to make use of their coding skills and are seeking a platform to do so. With varied events under it, it’s a perfect challenge for beginners to advanced coders!",
+    imageUrl:
+      "https://images.pexels.com/photos/5380605/pexels-photo-5380605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 11,
+    category: "technical",
+    title: "CHECKERED FLAG",
+    description:
+      "The 'Checkered Flag' event is a great opportunity for automotive enthusiasts to showcase their knowledge and skills. It consists of three sub-events: Disassemble, Auto Quiz, and Idea Presentation. The Disassemble event involves assembling and disassembling mechanical components, while the Auto Quiz challenges participants' knowledge of the automobile world. The Idea Presentation is a team event that focuses on presenting ideas for the transition from combustion engine vehicles to electric vehicles in India. Overall, Checkered Flag is a fun and engaging way to test and showcase one's automotive knowledge and skills.",
+    imageUrl:
+      "https://images.pexels.com/photos/11527708/pexels-photo-11527708.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 12,
+    category: "technical",
+    title: "ROBOTICS RENAISSANCE",
+    description:
+      "Robotics is a fascinating field of engineering that is becoming increasingly popular in our modern world. From industrial applications to more consumer-focused applications, robotics is changing the way we interact with and use technology. Robotics Renaissance under OJASS 2023 invites newcomers and pupils of Robotics and Automation to explore the possibilities of robotics and discover the potential of robots to revolutionize the way we live.",
+    imageUrl:
+      "https://images.pexels.com/photos/2085831/pexels-photo-2085831.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 13,
+    category: "entrepreneurial",
+    title: "CASE TRIVIA",
+    description:
+      "The business world is home to a boatload of opportunities with new opportunities arising every day. Analytics & Consulting Club of NIT Jamshedpur brings real-world business scenarios for people to test their business acumen. OJASS’23 makes it possible with the inclusion of a dedicated event section on the business world, the Case Trivia. So, bring your best solutions forward to excel at our event.",
+    imageUrl:
+      "https://images.pexels.com/photos/4792282/pexels-photo-4792282.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 14,
+    category: "entrepreneurial",
+    title: "PRODUS",
+    description:
+      "Produs is an event that encompasses a complete pre-exposure to an industrial lifestyle organized under OJASS – The annual Techno-Management Fest of NIT Jamshedpur. It includes all of the key elements of the industrial sector, including design, data analysis, marketing issues, and solutions. So be ready to experience the thrill of industrial exposure!",
+    imageUrl:
+      "https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 15,
+    category: "technical",
+    title: "NO GROUND ZONE",
+    description:
+      "Calling all RC plane enthusiasts! Get ready for an adrenaline-packed event that will take your flying skills to new heights. Team Phoenix presents 'No Ground Zone' organsied under OJASS’23, brings the flagship event for RC planes, featuring two exciting competitions - 'MICAV' and 'Touch Down the Plane'. This event is sure to make the skies come alive.",
+    imageUrl:
+      "https://images.pexels.com/photos/4447221/pexels-photo-4447221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 16,
+    category: "technical",
+    title: "RISE OF MACHINES",
+    description:
+      "It is impossible to imagine our daily life without machines. If the world has advanced technologically, the contribution of machines cannot be undermined. RISE OF MACHINES organized under OJASS pays tribute to the very core of Mechanical Engineering. This event calls out to all the students to let their passion for machines flow!",
+    imageUrl:
+      "https://images.pexels.com/photos/1087083/pexels-photo-1087083.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 17,
+    category: "miscellaneous",
+    title: "ARMAGEDDON",
+    description:
+      "Armageddon is the ultimate battleground for the brightest minds in the world of esports. This event is a celebration of the skill, dedication, and passion that drives the esports community. Don't miss out on the opportunity to be part of the action, and witness the sheer brilliance and passion of the next generation of esports superstars.",
+    imageUrl:
+      "https://images.pexels.com/photos/9072394/pexels-photo-9072394.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 19,
+    category: "school",
+    title: "JUNKYARD WARS",
+    description:
+      "“Education is not learning of facts but training the mind to think”. OJASS 23 brings an eminent opportunity for students to exhibit its technical extravaganza, entrepreneurial innovations and their creativity. With the prime motive of nurturing cognitive skills and wide exposure to school students here OJASS 23 we bring gaming events, photography event, film making events, junkyard wars and elevator pitch. It will be a perfect platform to grow and learn by participating in the event. Grab this opportunity bestowed on you!!",
+    imageUrl:
+      "https://images.pexels.com/photos/3401403/pexels-photo-3401403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 20,
+    category: "school",
+    title: "ELEVATOR PITCH",
+    description:
+      "“Education is not learning of facts but training the mind to think”. OJASS 23 brings an eminent opportunity for students to exhibit its technical extravaganza, entrepreneurial innovations and their creativity. With the prime motive of nurturing cognitive skills and wide exposure to school students here OJASS 23 we bring gaming events, photography event, film making events, junkyard wars and elevator pitch. It will be a perfect platform to grow and learn by participating in the event. Grab this opportunity bestowed on you!!",
+    imageUrl:
+      "https://images.pexels.com/photos/3401403/pexels-photo-3401403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 21,
+    category: "school",
+    title: "General Quiz with a Scientific Twist - A School Quiz",
+    description:
+      "“Education is not learning of facts but training the mind to think”. OJASS 23 brings an eminent opportunity for students to exhibit its technical extravaganza, entrepreneurial innovations and their creativity. With the prime motive of nurturing cognitive skills and wide exposure to school students here OJASS 23 we bring gaming events, photography event, film making events, junkyard wars and elevator pitch. It will be a perfect platform to grow and learn by participating in the event. Grab this opportunity bestowed on you!!",
+    imageUrl:
+      "https://images.pexels.com/photos/207756/pexels-photo-207756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 22,
+    category: "school",
+    title: "NCFF",
+    description:
+      "Photography is the art of teleporting the past into the future. Phocus presents the National College Film Festival - a combination of events, from filmmaking, reel making, after movie to Ad making. So bring out your perfect angles and creativity and participate in this event OJASS’23, filled lights, cameras and passion!",
+    imageUrl:
+      "https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: 23,
+    category: "school",
+    title: "ARMAGEDDON",
+    description:
+      "Armageddon is the ultimate battleground for the brightest minds in the world of esports. This event is a celebration of the skill, dedication, and passion that drives the esports community. Don't miss out on the opportunity to be part of the action, and witness the sheer brilliance and passion of the next generation of esports superstars.",
+    imageUrl:
+      "https://images.pexels.com/photos/9072394/pexels-photo-9072394.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+];
